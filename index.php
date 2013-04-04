5e49fea2a2dd23f2df74272ff409076202f7850d
ccog <cogan.cam@gmail.com>
2013-04-04 12:36:02 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
 $current_count = system("python check_num.cgi");
+echo " people found out about this cool website before you. Poser.\n"
 system("python increment.cgi");
+system("python time_check.cgi");
 ?>
